2ac39203876d151f4e748c04df9d211e28aee418
avery <grimm4253@gmail.com>
2025-06-02 20:03:04 -0700

    NEW

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
-const user = require("../models/user");
+const User = require("../models/user");
 
 const getUsers = (req, res) => {
-  user.find({})
+  User.find({})
     .then((users) => {
       res.status(200).send(users);
     })
@@ -14,14 +14,38 @@ const getUsers = (req, res) => {
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
 
-  user.create({ name, avatar })
+  User.create({ name, avatar })
     .then((user) => {
       res.status(201).send(user);
     })
     .catch((error) => {
       console.log(error);
+      if (error.name === "ValidationError") {
+        return res.status(400).send({ message: error.message });
+      }
       return res.status(500).send({ message: error.message });
     });
-}
+};
 
-module.exports = { getUsers, createUser };
+const getUser = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      return res.status(200).send(user);
+    })
+    .catch((error) => {
+      console.log(error);
+      if (error.name === "DocumentNotFoundError") {
+        return res.status(400).send({ message: error.message });
+      } else if (error.name === "CastError") {
+        return res.status(400).send({ message: error.message });
+      }
+      return res.status(500).send({ message: error.message });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
