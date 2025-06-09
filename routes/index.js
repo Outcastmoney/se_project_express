4a322b3d32c6e383ad8733573dc42f85833372a6
avery <grimm4253@gmail.com>
2025-06-09 09:30:04 -0700

    NEW

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const auth = require('../middlewares/auth');
 const { login, createUser } = require("../controllers/users");
 
 router.post("/signin", login);
-router.post("/signup", createUser);
+router.post("/users", createUser);
 router.use("/users", auth, usersRouter);
 router.use('/items', clothingItem);
 
